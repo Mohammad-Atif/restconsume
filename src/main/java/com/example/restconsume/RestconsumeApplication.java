2d724b99ea8a3eae54e6d58f6e113ebc27637296
atif9 <mohammadatif92080@gmail.com>
2022-07-02 23:05:03 +0530

    created domain class to contain data we need

--- src/main/java/com/example/restconsume/RestconsumeApplication.java
+++ src/main/java/com/example/restconsume/RestconsumeApplication.java
@@ -3,7 +3,9 @@ package com.example.restconsume;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+/*
+	This application is to learn about how to consume RESTful services using spring
+ */
 @SpringBootApplication
 public class RestconsumeApplication {
 
