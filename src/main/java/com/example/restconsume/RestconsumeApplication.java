7d4b9aef6af55f00126cc072f7fa7c9e6eed42ee
atif9 <mohammadatif92080@gmail.com>
2022-07-02 23:14:28 +0530

    added RestTemplate and CommandLineRunner

--- src/main/java/com/example/restconsume/RestconsumeApplication.java
+++ src/main/java/com/example/restconsume/RestconsumeApplication.java
@@ -1,7 +1,12 @@
 package com.example.restconsume;
 
+import com.example.restconsume.quote.Quote;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 /*
 	This application is to learn about how to consume RESTful services using spring
@@ -13,4 +18,26 @@ public class RestconsumeApplication {
 		SpringApplication.run(RestconsumeApplication.class, args);
 	}
 
+	/*
+		Now we need
+		1=  RestTemplate which uses Jackson JSON processing library to process the incoming data
+		2= CommandLineRunner that runs the RestTempelate(and, consequently fetches our quotation) on startup
+	 */
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder)
+	{
+		return builder.build();
+	}
+
+	@Bean
+	public CommandLineRunner run(RestTemplate restTemplate) throws  Exception
+	{
+		return args -> {
+			Quote quote = restTemplate.getForObject(
+					"https://quoters.apps.pcfone.io/api/random",Quote.class
+			);
+//			System.out.println(quote);
+		};
+	}
 }
